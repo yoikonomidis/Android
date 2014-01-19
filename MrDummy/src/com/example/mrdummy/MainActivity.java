67eb7850af59cc301556de409a1413633bdd70b0
ktsitsikas <ktsitsikas@gmail.com>
2014-01-19 20:21:21 +0100

    The everything all

--- MrDummy/src/com/example/mrdummy/MainActivity.java
+++ MrDummy/src/com/example/mrdummy/MainActivity.java
@@ -1,20 +1,54 @@
 package com.example.mrdummy;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.IntentSender;
+import android.content.res.Configuration;
 import android.view.Menu;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity {
+@SuppressLint("NewApi")
+public class MainActivity extends FragmentActivity implements
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener, LocationListener {
 
+	private GoogleMap googleMap;
 	private static final String TAG = "MainActivity";
-	//private static String urlget = "http://192.168.2.5:3000/emplist";
-	//private static String urlpost = "http://192.168.2.5:3000/addemp";
+	//private static String urlget = "http://192.168.2.6:3000/updateUserLocation";
+	//private static String urlpost = "http://192.168.2.6:3000/updateVehicleLocation";
 	private static EditText editTextIP, editTextPort, editTextFunction;
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 90000;
+    //add button that changes this
+    boolean mUpdatesRequested = true;
+    private LocationClient mLocationClient;
+    private LocationRequest mLocationRequest;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +57,6 @@ public class MainActivity extends FragmentActivity {
 		editTextIP = (EditText) this.findViewById(R.id.editTextIP);
 		editTextPort = (EditText) this.findViewById(R.id.editTextPort);
 		editTextFunction = (EditText) this.findViewById(R.id.editTextFunction);
-		Log.v(TAG,"Start Sensing!");
 	}
 
 	@Override
@@ -33,22 +66,210 @@ public class MainActivity extends FragmentActivity {
 		return true;
 	}
 	
-	public void startGeting(View view) {
-	    // Do something in response to button	
-		String ip, port, function;
-		ip = editTextIP.getText().toString();
-		port = editTextPort.getText().toString();
-		function = editTextFunction.getText().toString();
-		new Rester().execute("GET", ip, port, function);
+	public void startSensing(View view) {
+		Log.v(TAG,"Start Sensing!");
+        // Create a new global location parameters object
+        mLocationRequest = LocationRequest.create();
+
+        /*
+         * Set the update interval
+         */
+        mLocationRequest.setInterval(0);
+        mLocationRequest.setSmallestDisplacement(0);
+
+        // Use high accuracy
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // Set the interval ceiling 
+        mLocationRequest.setFastestInterval(0);
+
+        // Note that location updates are off until the user turns them on
+        mUpdatesRequested = true;
+
+        /*
+         * Create a new location client, using the enclosing class to
+         * handle callbacks.
+         */
+        mLocationClient = new LocationClient(this, this, this);
+        
+    	Log.v(TAG,"Start getting location!");
+        mUpdatesRequested = true;
+
+        if (servicesConnected()) {
+        	Log.v(TAG,"Services connected!");
+        	mLocationClient.connect();
+        }
 	}
 	
-	public void startPosting(View view) {
+	public void stopSensing(View view) {
 	    // Do something in response to button	
-		String ip, port, function;
-		ip = editTextIP.getText().toString();
-		port = editTextPort.getText().toString();
-		function = editTextFunction.getText().toString();
-		new Rester().execute("POST", ip, port, function);
+		Log.v(TAG,"Stop Sensing!");
+		mLocationClient.disconnect();
 	}
 
+	// Check if Google Play Services are available
+	@SuppressLint("NewApi")
+	public static class ErrorDialogFragment extends DialogFragment {
+        private Dialog mDialog;
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
+    }
+	
+    private boolean servicesConnected() {
+
+        // Check that Google Play services is available
+        int resultCode =
+                GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            // Continue
+        	Log.v(TAG,"Connected!");
+            return true;
+        // Google Play services was not available for some reason
+        } else {
+            // Display an error dialog
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0);
+            if (dialog != null) {
+                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+                errorFragment.setDialog(dialog);
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                * Thrown if Google Play services canceled the original
+                * PendingIntent
+                */
+            } catch (IntentSender.SendIntentException e) {
+
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+
+            // If no resolution is available, display a dialog to the user with the error.
+        }
+    }
+	
+    private void startPeriodicUpdates() {
+
+        mLocationClient.requestLocationUpdates(mLocationRequest, this);
+    }
+
+    @Override
+    protected void onStart() {
+    	super.onStart();
+    }
+    
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        if (mUpdatesRequested) {
+        	Log.v(TAG,"Start getting updates!");
+            startPeriodicUpdates();
+        }
+        
+        //TODO: Separate function
+        setContentView(R.layout.activity_map);
+        // Getting reference to the SupportMapFragment of activity_main.xml
+        SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        // Getting GoogleMap object from the fragment
+        googleMap = fm.getMap();
+        // Enabling MyLocation Layer of Google Map
+        googleMap.setMyLocationEnabled(true);
+    }
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
+	}
+
+    @Override
+    public void onLocationChanged(Location location) {
+    	double latitude, longitude;
+    	
+    	latitude = location.getLatitude();
+    	longitude = location.getLongitude();
+        String msg = "Updated Location: " +
+                Double.toString(latitude) + "," +
+                Double.toString(longitude);
+
+        LatLng latLng = new LatLng(latitude, longitude);
+        googleMap.addMarker(new MarkerOptions().position(latLng).title("cur_pos"));
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+        .target(latLng)      		// Sets the center of the map to Mountain View
+        .zoom(17)                   // Sets the zoom
+        .bearing(90)                // Sets the orientation of the camera to east
+        .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+        .build();                   // Creates a CameraPosition from the builder
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        
+        JSONObject postData = new JSONObject();
+        JSONObject connectionData = new JSONObject();
+        try {
+        	//Create json for postData
+        	postData.put("id", 4);
+        	postData.put("name", "220");
+        	
+        	JSONObject loc = new JSONObject();
+        	loc.put("latitude", latitude);
+        	loc.put("longitude", longitude);
+	        postData.put("location", loc);
+	        
+	        //Create json for connectionData
+	        connectionData.put("method", "POST");
+	        connectionData.put("ip", editTextIP.getText().toString());
+	        connectionData.put("port", editTextPort.getText().toString());
+	        connectionData.put("function", editTextFunction.getText().toString());
+        } catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+		Rester rester = new Rester();
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+			rester.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, connectionData, postData);
+		else
+			rester.execute(connectionData, postData);
+        Log.v(TAG,"Location found:"+msg);
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+    
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+    }
+    
 }
--- MrDummy/src/com/example/mrdummy/Rester.java
+++ MrDummy/src/com/example/mrdummy/Rester.java
@@ -21,81 +21,87 @@ import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.os.AsyncTask;
 import android.util.Log;
 
-public class Rester extends AsyncTask<String, String, String> {
+public class Rester extends AsyncTask<JSONObject, Void, String> {
 
 	private static final String TAG = "Rester";
 	private static InputStream is = null;
 	private static int timeoutConnection = 3500;
 	@Override
-	protected String doInBackground(String... params) {
+	protected String doInBackground(JSONObject... params) {
 		// TODO Auto-generated method stub
 		Log.v(TAG, "About to send request...");
-		dummyRest(params[0], params[1], params[2], params[3]);
+		dummyRest(params[0], params[1]);
 		return null;
 	}
 	
-	public void dummyRest(String method, String ip, String port, String function) {
-		List<NameValuePair> params = new ArrayList<NameValuePair>();
-		HttpParams httpParameters = new BasicHttpParams();
-		HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
-		DefaultHttpClient httpClient = new DefaultHttpClient(httpParameters);
-		String url = "http://"+ip+":"+port+"/" + function;
-		Log.v(TAG, "URL: " + url);
-		
+	public void dummyRest(JSONObject connectionData, JSONObject postData) {
+		String url, method, ip, port, function;
 		try {
-			//add timeout
-			if (method == "GET") {
-				Log.v(TAG, "Sending GET request...");
-	            String paramString = URLEncodedUtils.format(params, "utf-8");
-	            url += "?" + paramString;
-	            HttpGet httpGet = new HttpGet(url);
-	
-	            HttpResponse httpResponse = httpClient.execute(httpGet);
-	            HttpEntity httpEntity = httpResponse.getEntity();
-	            is = httpEntity.getContent();
+			method = connectionData.get("method").toString();
+			ip = connectionData.get("ip").toString();
+			port = connectionData.get("port").toString();
+			function = connectionData.get("function").toString();
+			url = "http://"+ip+":"+port+"/" + function;
+			Log.v(TAG, "URL: " + url);
+			
+			List<NameValuePair> params = new ArrayList<NameValuePair>();
+			HttpParams httpParameters = new BasicHttpParams();
+			HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
+			DefaultHttpClient httpClient = new DefaultHttpClient(httpParameters);
+			
+			try {
+				//add timeout
+				if (method == "GET") {
+					Log.v(TAG, "Sending GET request...");
+		            String paramString = URLEncodedUtils.format(params, "utf-8");
+		            url += "?" + paramString;
+		            HttpGet httpGet = new HttpGet(url);
+		
+		            HttpResponse httpResponse = httpClient.execute(httpGet);
+		            HttpEntity httpEntity = httpResponse.getEntity();
+		            is = httpEntity.getContent();
+				}
+				else if (method == "POST") {
+					Log.v(TAG, "Sending POST request...");
+	                HttpPost httpPost = new HttpPost(url);
+	                
+	                StringEntity se = new StringEntity(postData.toString());  
+	                Log.v(TAG,"Data sent: " + postData);
+	                se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+	                httpPost.setEntity(se);
+	                
+	                HttpResponse httpResponse = httpClient.execute(httpPost);
+	                HttpEntity httpEntity = httpResponse.getEntity();
+	                is = httpEntity.getContent();
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			else if (method == "POST") {
-				Log.v(TAG, "Sending POST request...");
-                HttpPost httpPost = new HttpPost(url);
-                
-                JSONObject json = new JSONObject();
-
-                json.put("id", "41");
-                json.put("firstName", "Stelios");
-                json.put("lastName", "Kouloglou");
-                json.put("managerId", "42");
-                StringEntity se = new StringEntity(json.toString());  
-                Log.v(TAG,"Data sent: " + json);
-                se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-                httpPost.setEntity(se);
-                
-                HttpResponse httpResponse = httpClient.execute(httpPost);
-                HttpEntity httpEntity = httpResponse.getEntity();
-                is = httpEntity.getContent();
-			}
-		} catch (Exception e) {
+			
+			try {
+	            BufferedReader reader = new BufferedReader(new InputStreamReader(
+	                    is, "iso-8859-1"), 8);
+	            StringBuilder sb = new StringBuilder();
+	            String line = null;
+	            while ((line = reader.readLine()) != null) {
+	                sb.append(line + "\n");
+	            }
+	            is.close();
+	            Log.v(TAG, "Response from the server " + sb.toString());
+	        } catch (Exception e) {
+	            Log.e("Buffer Error", "Error converting result " + e.toString());
+	        }
+		} catch (JSONException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
-		
-		try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    is, "iso-8859-1"), 8);
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-            is.close();
-            Log.v(TAG, "Response from the server " + sb.toString());
-        } catch (Exception e) {
-            Log.e("Buffer Error", "Error converting result " + e.toString());
-        }
 	}
 
 }
