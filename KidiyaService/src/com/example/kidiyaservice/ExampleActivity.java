24071a2b937da92cdbfaa9d626b8c8f3a00c649e
yoikonomidis <yannisoikonomidis@gmail.com>
2014-03-09 21:34:51 +0100

    Add basic UI, one start view, one map view.

--- KidiyaService/src/com/example/kidiyaservice/ExampleActivity.java
+++ KidiyaService/src/com/example/kidiyaservice/ExampleActivity.java
@@ -1,22 +1,102 @@
 package com.example.kidiyaservice;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
 
 /*
  * Dummy activity that connects ExampleApp.
  */
 public class ExampleActivity extends Activity {
-     @Override
-     public void onCreate(Bundle savedInstanceState) {
-         super.onCreate(savedInstanceState);
-         setContentView(R.layout.application_main);
-         ExampleApp app = (ExampleApp) getApplication();
-    }
-     
-     @Override
-     protected void onResume() {
-         super.onResume();
-     }
+	
+	private Context m_context;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.application_main);
+//		setContentView(R.layout.map_layout);
+		m_context = this;
+		
+		GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new MyAdapter(m_context));
+        gridview.setNumColumns(1);
+        
+		ExampleApp app = (ExampleApp) getApplication();
+		
+		gridview.setOnItemClickListener(new OnItemClickListener() {
+	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	        	Intent intent = new Intent(m_context, MapActivity.class);
+	            
+//	            String message = editText.getText().toString();
+//	            intent.putExtra(EXTRA_MESSAGE, message);
+	            startActivity(intent);
+
+	        }
+	    });
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+}
+
+class MyAdapter extends BaseAdapter {
+
+	private Context m_context;
+
+	public MyAdapter(Context c) {
+		m_context = c;
+	}
+
+	@Override
+	public int getCount() {
+		return m_menuIds.length;
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		return m_menuIds[arg0];
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		return arg0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		View grid;
+
+		if(convertView==null){
+			grid = new View(m_context);
+//			LayoutInflater inflater = getLayoutInflater();
+			LayoutInflater inflater = (LayoutInflater) m_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			grid=inflater.inflate(R.layout.grid_layout, parent, false);
+		}else{
+			grid = (View)convertView;
+		}
+
+		ImageView imageView = (ImageView)grid.findViewById(R.id.image);
+		imageView.setImageResource(m_menuIds[position]);
+
+		return grid;
+	}
+	
+	private Integer[] m_menuIds = {
+			R.drawable.map_icon
+	};
 }
---
+++ KidiyaService/src/com/example/kidiyaservice/MapActivity.java
@@ -0,0 +1,65 @@
+package com.example.kidiyaservice;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.annotation.TargetApi;
+import android.os.Build;
+
+public class MapActivity extends FragmentActivity {
+
+	private GoogleMap m_googleMap;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map);
+		// Show the Up button in the action bar.
+		setupActionBar();
+		
+		// Getting reference to the SupportMapFragment of activity_main.xml
+        SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        // Getting GoogleMap object from the fragment
+        m_googleMap = fm.getMap();
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.map, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
